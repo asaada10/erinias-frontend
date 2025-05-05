3738d4ffbb4c8d4baac3fdf09a5b4c0048a3d694
Ayoub <ayoubsaada10@gmail.com>
2025-05-05 15:15:42 +0200

    Mejorar el Resizable en diseño móvil.

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -19,30 +19,39 @@
 	<!-- Main Content -->
 	<div class="flex h-[calc(100vh-64px)]">
 		<!-- Sidebar -->
-		<Resizable.PaneGroup direction="horizontal">
-			<Resizable.Pane
-				class="md:relative md:flex md:w-80 md:flex-col md:border-r md:border-gray-200 dark:bg-gray-900/90 md:dark:border-gray-700"
-				defaultSize={20}
-			>
+		<div class="hidden w-full md:flex">
+			<Resizable.PaneGroup direction="horizontal" class="flex w-full">
+				<Resizable.Pane
+					class="relative flex w-80 flex-col border-r border-gray-200 dark:border-gray-700 dark:bg-gray-900/90"
+					defaultSize={20}
+				>
+					<Sidebar {layout} {sidebarOpen} {toggleSidebar} />
+				</Resizable.Pane>
+				<Resizable.Handle withHandle />
+				<Resizable.Pane class="flex flex-1 flex-col">
+					{@render layout.chatHeader?.()}
+					<div class="flex-1 overflow-y-auto p-4">
+						{@render layout.chat?.()}
+					</div>
+					{@render layout.chatInput?.()}
+				</Resizable.Pane>
+			</Resizable.PaneGroup>
+		</div>
+
+		<!-- Mobile: condicional según sidebarOpen -->
+		{#if sidebarOpen}
+			<div class="flex w-full md:hidden">
 				<Sidebar {layout} {sidebarOpen} {toggleSidebar} />
-			</Resizable.Pane>
-			<Resizable.Handle withHandle />
-
-			<!-- Chat Area -->
-			<Resizable.Pane class="flex flex-1 flex-col">
-				<!-- Chat Header -->
+			</div>
+		{:else}
+			<div class="flex w-full flex-col md:hidden">
 				{@render layout.chatHeader?.()}
-
-				<!-- Messages -->
 				<div class="flex-1 overflow-y-auto p-4">
 					{@render layout.chat?.()}
 				</div>
-
-				<!-- Chat Input -->
-
 				{@render layout.chatInput?.()}
-			</Resizable.Pane>
-		</Resizable.PaneGroup>
+			</div>
+		{/if}
 	</div>
 </div>
 {@render children?.()}
