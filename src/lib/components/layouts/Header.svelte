a79ca73d2d1bc2782b47fff3fdea3f79d0b090f7
Ayoub <ayoubsaada10@gmail.com>
2025-05-16 16:06:09 +0000

    Eliminar botones de mensajes directos y grupos en el encabezado, simplificando la interfaz de usuario.

--- src/lib/components/layouts/Header.svelte
+++ src/lib/components/layouts/Header.svelte
@@ -29,57 +29,10 @@
 		<button class="hidden p-1 md:block" aria-label="Logo">
 			<img src="/erinias.svg" alt="Icon" class="h-6 w-6" />
 		</button>
-		<button class="flex items-center space-x-1 p-1 md:flex">
-			<svg class="h-5 w-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<path
-					d="M8 10H16M8 14H16M19 5H5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V7C21 5.89543 20.1046 5 19 5Z"
-					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				/>
-			</svg>
-			<span class="text-sm font-medium">Direct Messages</span>
-		</button>
-		<button class="flex items-center space-x-1 p-1 md:flex">
-			<svg class="h-5 w-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<path
-					d="M17 20H7C4.79086 20 3 18.2091 3 16V8C3 5.79086 4.79086 4 7 4H17C19.2091 4 21 5.79086 21 8V16C21 18.2091 19.2091 20 17 20Z"
-					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				/>
-				<path
-					d="M12 10C13.1046 10 14 9.10457 14 8C14 6.89543 13.1046 6 12 6C10.8954 6 10 6.89543 10 8C10 9.10457 10.8954 10 12 10Z"
-					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				/>
-				<path
-					d="M16 15C16 13.3431 14.2091 12 12 12C9.79086 12 8 13.3431 8 15"
-					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				/>
-			</svg>
-			<span class="text-sm font-medium">Groups</span>
-		</button>
+
 	</div>
 	<div class="flex items-center space-x-2">
-		<button class="hidden p-1 md:block" aria-label="Menu">
-			<svg class="h-5 w-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<path
-					d="M9.09 9C9.3251 8.33167 9.78915 7.76811 10.4 7.40913C11.0108 7.05016 11.7289 6.91894 12.4272 7.03871C13.1255 7.15849 13.7588 7.52152 14.2151 8.06353C14.6713 8.60553 14.9211 9.29152 14.92 10C14.92 12 11.92 13 11.92 13M12 17H12.01M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
-					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				/>
-			</svg>
-		</button>
+
 		<DropdownMenu.Root>
 			<DropdownMenu.Trigger>Profile</DropdownMenu.Trigger>
 			<DropdownMenu.Content>
